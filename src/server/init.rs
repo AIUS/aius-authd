0b61e3d8739b6d82b6a63fb2a79e56675e369d49
Quentin Gliech <quentingliech@gmail.com>
2017-01-08 21:30:38 +0100

    Basic error catching

--- src/server/init.rs
+++ src/server/init.rs
@@ -3,13 +3,14 @@ use iron::Listening;
 use iron::Handler;
 use iron::prelude::*;
 
-use server::middlewares::ConfigBeforeMiddleware;
+use server::middlewares::{ConfigBeforeMiddleware, ErrorAfterMiddleware};
 use config::Config;
 
 pub fn start<H: Handler>(config: Config, handler: H) -> HttpResult<Listening> {
     // Setup the middleware chain
     let mut chain = Chain::new(handler);
     chain.link_before(ConfigBeforeMiddleware::new(config.clone()));
+    chain.link_after(ErrorAfterMiddleware {});
 
     // Spin up the server
     Iron::new(chain)
--- src/server/middlewares.rs
+++ src/server/middlewares.rs
@@ -1,6 +1,9 @@
 use iron::prelude::*;
 use iron::typemap;
-use iron::BeforeMiddleware;
+use iron::status;
+use iron::{AfterMiddleware, BeforeMiddleware};
+use redis::RedisError;
+use openldap::errors::LDAPError;
 use config;
 
 pub struct CurrentConfig;
@@ -22,5 +25,18 @@ impl BeforeMiddleware for ConfigBeforeMiddleware {
     fn before(&self, req: &mut Request) -> IronResult<()> {
         req.extensions.insert::<CurrentConfig>(self.config.clone());
         Ok(())
+    }
+}
+
+pub struct ErrorAfterMiddleware;
+impl AfterMiddleware for ErrorAfterMiddleware {
+    fn catch(&self, _: &mut Request, err: IronError) -> IronResult<Response> {
+        if err.error.is::<RedisError>() {
+            return Ok(Response::with((status::ServiceUnavailable, "Redis Error")))
+        } else if err.error.is::<LDAPError>() {
+            return Ok(Response::with((status::ServiceUnavailable, "LDAP Error")))
+        } else {
+            Err(err)
+        }
     }
 }
